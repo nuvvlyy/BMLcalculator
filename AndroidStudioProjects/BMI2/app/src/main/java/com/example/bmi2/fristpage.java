973bf8b6d6686f28c6f516d1866b76ed9cc4f6a4
nuvvlyy <nutnutnut_1998@hotmail.com>
2018-10-13 20:41:12 +0700

    frist

---
+++ AndroidStudioProjects/BMI2/app/src/main/java/com/example/bmi2/fristpage.java
@@ -0,0 +1,13 @@
+package com.example.bmi2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class fristpage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fristpage);
+    }
+}
